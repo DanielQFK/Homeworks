909e327e3cf2a0fbbe017a37939118ee38595dd2
DanielQFK <DanialQueen2004@gmail.com>
2021-06-23 10:07:02 -0400

    def-import homework...

---
+++ Homework1_def.py
@@ -0,0 +1,12 @@
+# Hi , I hope you be alright , as you see this is my homework of def and import but this is only "def" part
+# and the import part is in the next file
+#in these files , I wrote a program to calculate the exponent , but I put it in def 
+#and in next file , I used this def by importing ...
+def Def():
+    first_number = int(input("Enter your first number > "))
+    second_number = int(input("Enter your second number > "))
+    #to get number , we use int-input
+    exponent = first_number**second_number 
+    return exponent
+    #we return the variable to be able to print
+#It was the first part(Just def part) the next file is the continue of this file...
---
+++ Homework2_import.py
@@ -0,0 +1,11 @@
+import Homework1_def
+#In here , we import the name of our file we wnat to import the def from
+
+result = Homework1_def.Def() 
+#Because it has a value , we give it a name(Variable)
+print(result)
+#and at last , we print the variable
+#Finish :)
+
+
+
